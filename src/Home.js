76798efd9cbc205ec34991cb45ce52152342e6a7
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-21 14:44:10 -0300

    corazones

--- src/Home.js
+++ src/Home.js
@@ -58,15 +58,41 @@ const Home = () => {
   {limitar6.map(creacion => (
     <div key={creacion.id} className="col-md-4">
       <div className="card" style={styles.card}>
-        <img src={creacion.imagen} className="card-img-top" alt={creacion.titulo} style={{ height: '200px', objectFit: 'cover', borderRadius: '10px' }} />
+        <img src={creacion.imagen} className="card-img-top" alt={creacion.titulo} style={{ height: '200px', width: '100%', objectFit: 'cover', borderRadius: '10px' }} />
         <div className="card-body">
           <h5 className="card-title" style={styles.cardTitle}>{creacion.titulo}</h5>
-          <a href={creacion.url} className="btn btn-primary" style={styles.cardButton}>Ver más</a>
+
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '1%' }}>
+            <a href={creacion.url} className="btn btn-primary" style={styles.cardButton}>
+              Ver más
+            </a>
+            <div className="heart-container" title="Like">
+              <input type="checkbox" className="checkbox" id={`Give-It-An-Id-${creacion.id}`} />
+              <div className="svg-container">
+                <svg viewBox="0 0 24 24" className="svg-outline" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z" />
+                </svg>
+                <svg viewBox="0 0 24 24" className="svg-filled" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Z" />
+                </svg>
+                <svg className="svg-celebrate" width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                  <polygon points="10,10 20,20"></polygon>
+                  <polygon points="10,50 20,50"></polygon>
+                  <polygon points="20,80 30,70"></polygon>
+                  <polygon points="90,10 80,20"></polygon>
+                  <polygon points="90,50 80,50"></polygon>
+                  <polygon points="80,80 70,70"></polygon>
+                </svg>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>  
+      </div>
     </div>
   ))}
 </div>
+
+
     </div>
   );
 };
@@ -75,7 +101,7 @@ const styles = {
   container: {
     padding: '80px',
     paddingTop: '110px',
-    backgroundColor: '#efe9f7', // Light gray background
+    backgroundColor: '#efe9f7', 
     borderRadius: '13px',
     boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
     textAlign: 'center',
@@ -85,13 +111,13 @@ const styles = {
   },
   subtitle: {
     fontSize: '20px',
-    color: '#fff', // White text
+    color: '#fff', 
     marginBottom: '10px',
     textTransform: 'uppercase',
-    padding: '10px 20px', // Added padding
-    background: 'linear-gradient(to right, #8b26b4, #336699)', // Gradient background
-    borderRadius: '15px', // Rounded corners
-    display: 'inline-block', // Display as inline block
+    padding: '10px 20px', 
+    background: 'linear-gradient(to right, #8b26b4, #336699)', 
+    borderRadius: '15px',
+    display: 'inline-block', 
   },
   title: {
     marginBottom: '20px',
